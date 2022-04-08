b10ef5aee54915703c2cf1a63a3193fddf31d667
0xshred <0xshred@protonmail.com>
2022-04-08 10:54:10 -0700

    BuiltByRaidGuild size

--- src/components/footers/Simple.tsx
+++ src/components/footers/Simple.tsx
@@ -15,7 +15,7 @@ export const FooterSimple = () => (
   >
     <Stack>
       <Stack direction="row" spacing="4" align="center" justify="space-between">
-        <BuiltByRaidGuild />
+        <BuiltByRaidGuild height="8em" width="8em" />
         <SocialMediaLinks />
       </Stack>
       <Copyright alignSelf={{ base: 'center', sm: 'start' }} />
